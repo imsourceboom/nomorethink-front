a54a044bfd9d9dad3c36467d614cedd2ce57a050
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-07 18:27:09 +0900

    feat: Add Telegram WebApp type definitions

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,13 @@ declare global {
                 ready: () => void;
                 expand: () => void;
                 requestFullscreen: () => void;
+                disableVerticalSwipes: () => void;
+                MainButton: {
+                    text: string;
+                    show: () => void;
+                    hide: () => void;
+                    onClick: (callback: () => void) => void;
+                };
             };
         };
     }
