f384870a7374a212259864a085d2566f26af52fc
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-07 18:27:21 +0900

    feat: Configure Next.js for Telegram WebApp integration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
     images: {
         domains: ['via.placeholder.com'], // 허용할 도메인 추가
     },
+    webpack: (config) => {
+        config.externals = [...(config.externals || []), { "telegram-web-app": "window.Telegram" }];
+        return config;
+    },
 };
 
 export default nextConfig;
