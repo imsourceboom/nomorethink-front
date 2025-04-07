b72f06be98cf4821202742cdfa2f43fc721b6274
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-07 21:28:55 +0900

    fix: Update global type declarations

--- global.d.ts
+++ global.d.ts
@@ -2,20 +2,20 @@
 /// <reference types="next/image-types/global" />
 
 // global.d.ts
-interface TelegramWebApp {
-    ready: () => void;
-    expand: () => void;
-    MainButton: {
-        text: string;
-        show: () => void;
-        hide: () => void;
-        onClick: (callback: () => void) => void;
-    };
-    onEvent: (eventType: string, callback: () => void) => void;
-    offEvent: (eventType: string, callback: () => void) => void;
-}
-
 declare global {
+    interface TelegramWebApp {
+        ready: () => void;
+        expand: () => void;
+        MainButton: {
+            text: string;
+            show: () => void;
+            hide: () => void;
+            onClick: (callback: () => void) => void;
+        };
+        onEvent: (eventType: string, callback: () => void) => void;
+        offEvent: (eventType: string, callback: () => void) => void;
+    }
+
     interface Window {
         Telegram?: {
             WebApp: TelegramWebApp;
