0f64c1f5189f95a363e7495d21db53bc176e7eff
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-07 21:30:36 +0900

    fix: Move global type declarations to app/types directory

---
+++ app/types/global.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare global {
+  interface TelegramWebApp {
+    ready: () => void;
+    expand: () => void;
+    MainButton: {
+      text: string;
+      show: () => void;
+      hide: () => void;
+      onClick: (callback: () => void) => void;
+    };
+    onEvent: (eventType: string, callback: () => void) => void;
+    offEvent: (eventType: string, callback: () => void) => void;
+  }
+
+  interface Window {
+    Telegram?: {
+      WebApp: TelegramWebApp;
+    };
+  }
+}
+
+export {}; 
