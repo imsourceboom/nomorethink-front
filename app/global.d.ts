6d90803a4043f37fd598ea7d54508463f0f3712d
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-08 10:48:06 +0900

    fix: Telegram WebApp 타입 정의 수정

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,21 +1,20 @@
 // Telegram WebApp 타입 정의
-interface TelegramWebApp {
-    ready: () => void;
-    expand: () => void;
-    requestFullscreen: () => void;
-    disableVerticalSwipes: () => void;
-    MainButton: {
-        text: string;
-        show: () => void;
-        hide: () => void;
-        onClick: (callback: () => void) => void;
-    };
-    onEvent: (eventType: string, callback: () => void) => void;
-    offEvent: (eventType: string, callback: () => void) => void;
-}
-
-// Window 인터페이스 확장
 declare global {
+    interface TelegramWebApp {
+        ready: () => void;
+        expand: () => void;
+        requestFullscreen: () => void;
+        disableVerticalSwipes: () => void;
+        MainButton: {
+            text: string;
+            show: () => void;
+            hide: () => void;
+            onClick: (callback: () => void) => void;
+        };
+        onEvent: (eventType: string, callback: () => void) => void;
+        offEvent: (eventType: string, callback: () => void) => void;
+    }
+
     interface Window {
         Telegram?: {
             WebApp?: TelegramWebApp;
