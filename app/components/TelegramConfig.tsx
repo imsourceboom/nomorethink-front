dfec17a7c2d61f62e4f6549e3bcd805b65bd69d2
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-09 14:20:03 +0900

    fix: 텔레그램 WebApp 타입 에러 수정

--- app/components/TelegramConfig.tsx
+++ app/components/TelegramConfig.tsx
@@ -8,12 +8,6 @@ export default function TelegramConfig() {
             if (window.Telegram?.WebApp) {
                 // 스와이프 비활성화
                 window.Telegram.WebApp.disableVerticalSwipes();
-                
-                // 배경색 설정 (선택사항)
-                window.Telegram.WebApp.setBackgroundColor('#000000');
-                
-                // 헤더 색상 설정 (선택사항)
-                window.Telegram.WebApp.setHeaderColor('#000000');
 
                 // 메인 버튼 숨기기 (선택사항)
                 window.Telegram.WebApp.MainButton.hide();
