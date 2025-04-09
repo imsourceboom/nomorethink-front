8d0fd6ddce928cd70fdc4516a21485cb1eb8f49d
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-09 13:18:52 +0900

    feat: 텔레그램 미니앱 수직 스와이프 비활성화 기능 추가

--- app/components/Template.tsx
+++ app/components/Template.tsx
@@ -2,9 +2,16 @@
 
 import { motion, AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Template({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
+
+    useEffect(() => {
+        if (window.Telegram?.WebApp) {
+            window.Telegram.WebApp.disableVerticalSwipes();
+        }
+    }, []);
 
     return (
         <AnimatePresence mode="wait">
