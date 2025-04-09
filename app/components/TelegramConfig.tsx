fbea374a5e38e367d29b4380a65ca4456411a308
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-09 14:28:16 +0900

    fix: TelegramConfig 컴포넌트를 순수 JavaScript 방식으로 수정

--- app/components/TelegramConfig.tsx
+++ app/components/TelegramConfig.tsx
@@ -1,14 +1,24 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useTelegram } from '../hooks/useTelegram';
+
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
 export default function TelegramConfig() {
-    const { initTelegram } = useTelegram();
-
     useEffect(() => {
-        initTelegram();
-    }, [initTelegram]);
+        if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
+            const tg = window.Telegram.WebApp;
+            
+            if (isMobileDevice()) {
+                tg.ready();
+                tg.expand();
+                tg.requestFullscreen();
+                tg.disableVerticalSwipes();
+            }
+        }
+    }, []);
 
     return null;
 } 
