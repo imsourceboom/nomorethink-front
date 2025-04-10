50f33fde4fdda46fea8680e2168c824dbb17da8c
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-10 16:13:56 +0900

    fix: ActionMeta 미사용 임포트 제거

--- app/(pages)/add/page.tsx
+++ app/(pages)/add/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Select, { StylesConfig, SingleValue, ActionMeta } from 'react-select';
+import Select, { StylesConfig, SingleValue } from 'react-select';
 import DatePicker from 'react-datepicker';
 import { registerLocale } from 'react-datepicker';
 import { ko } from 'date-fns/locale';
