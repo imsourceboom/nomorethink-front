c616fd28d48970d2e79598ef60fa4cd3f377e51b
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-21 20:44:13 +0900

    chore: commit all pending changes

--- app/(pages)/add/page.tsx
+++ app/(pages)/add/page.tsx
@@ -248,6 +248,4 @@ export default function AddPage() {
             </TelegramWrapper>
         </ErrorBoundary>
     );
-}
-
-<style jsx global>{` input[type=number]::-webkit-inner-spin-button, input[type=number]::-webkit-outer-spin-button { -webkit-appearance: none; margin: 0; } `}</style> 
+} 
