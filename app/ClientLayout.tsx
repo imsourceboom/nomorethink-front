17363ba3f6647128562ae016a0379692afd8a58a
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-21 19:20:54 +0900

    feat: 로딩/메인 화면 페이드 인/아웃 애니메이션 적용

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import FloatingMenu from './components/FloatingMenu';
 import Template from './components/Template';
 import Loading from './components/Loading';
@@ -41,20 +42,31 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
         };
     }, [initTelegram]);
 
-    // 클라이언트 마운트 전에는 Loading 스피너만 표시
-    if (!mounted) {
-        return <Loading />;
-    }
-
-    // 클라이언트 렌더링에서는 전체 UI 반환
+    // 로딩과 메인 UI에 페이드 애니메이션 적용
     return (
-        <>
-            <Loading />
-            <Prefetch />
-            <Template>
-                {children}
-            </Template>
-            <FloatingMenu />
-        </>
+        <AnimatePresence mode="wait">
+            {!mounted ? (
+                <motion.div
+                    key="loading"
+                    initial={{ opacity: 1 }}
+                    exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                >
+                    <Loading />
+                </motion.div>
+            ) : (
+                <motion.div
+                    key="main"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, transition: { duration: 0.5 } }}
+                    exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                >
+                    <Prefetch />
+                    <Template>
+                        {children}
+                    </Template>
+                    <FloatingMenu />
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 } 
