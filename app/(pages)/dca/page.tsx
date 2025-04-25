1170ed455274a3e5d06bb21d3eab41688925917b
imsourceboom <ecmascript.devloper@gmail.com>
2025-04-25 17:39:24 +0900

    feat: + 버튼 hover 스타일 및 기타 UI 변경

--- app/(pages)/dca/page.tsx
+++ app/(pages)/dca/page.tsx
@@ -45,9 +45,14 @@ export default function DcaPage() {
               <Link href="/dca/add">
                 <button
                   type="button"
-                  className="w-10 h-10 rounded-full border-2 flex items-center justify-center  border-[rgba(59,142,165,0.5)]"
+                  className="w-10 h-10 rounded-full border-2 flex items-center justify-center border-[rgba(59,142,165,0.5)] group hover:bg-[rgba(59,142,165,0.5)]"
                 >
-                  <svg className="w-6 h-6" style={{ color: 'rgba(59, 142, 165, 0.5)' }} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg
+                    className="w-6 h-6 text-[rgba(59,142,165,0.5)] group-hover:text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                   </svg>
                 </button>
